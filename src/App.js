5bf5341787eb1aa7e3c25bd9d972f09abc93acd0
erick1293 <116215531+erick1293@users.noreply.github.com>
2022-11-25 18:29:37 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Header } from "./Componentes/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Footer } from "./Componentes/Footer";
+import { Seccion1 } from "./Componentes/Seccion1";
+import { Seccion2 } from "./Componentes/Seccion2";
+import { Home } from "./Componentes/Home";
+import { Seccion3 } from "./Componentes/Seccion3";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "react-bootstrap/Nav";
+import "./assets/comp.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/seccion1" element={<Seccion1 />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/seccion2" element={<Seccion2 />} />
+          <Route path="/seccion3" element={<Seccion3 />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
